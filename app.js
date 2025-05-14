a57d18425f57b92c435a3361c0e958fce8509ecc
TongM28 <137702324+TongM28@users.noreply.github.com>
2025-05-14 19:35:17 +0700

    add port

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+const port = 8080;
+app.get("/", () => {
+  return "Hello World";
+});
+
+app.listen(8080, () => {
+  console.log(`Example app listening on port ${port}`);
+});
