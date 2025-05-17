d4d4cbfc12edeb1c66520f12c84e13e4fb6d08c5
TongM28 <137702324+TongM28@users.noreply.github.com>
2025-05-17 16:13:33 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
-// const express = require("express");
 import express from "express";
+require('dotenv').config()
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+//config view engine
+app.set('view engine', 'ejs');
+app.set('view', __dirname + './views/');
+console.log(__dirname + './views/')
+
 app.get("/", (req, res) => {
-  res.send("Hello World nodemon");
+  res.render("home.ejs");
 });
 
-app.get("/tongminhtest", (req, res) => {
-  res.send("hello minh");
+app.get("/acb", (req, res) => {
+  res.send("hello cbd"); 
+});
+
+app.get("/abc", (req, res) => {
+  res.send("hello abc");
 });
 
 app.listen(8080, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(__dirname + './views/')
 });
