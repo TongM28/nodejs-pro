19dba4c70c054d60ae1c4ca49d4ee541a51679cf
TongM28 <137702324+TongM28@users.noreply.github.com>
2025-05-20 18:08:34 +0700

    fix bug

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,20 @@
 import express from "express";
-require('dotenv').config()
-
+import 'dotenv/config';
+import webRouters from "./routes/web";
+ 
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 //config view engine
 app.set('view engine', 'ejs');
-app.set('view', __dirname + './views/');
-console.log(__dirname + './views/')
+app.set('views', __dirname + '/views');
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
+//config routes
+webRouters(app);
 
-app.get("/acb", (req, res) => {
-  res.send("hello cbd"); 
-});
+//config static files: images/css/js
+app.use(express.static('public'));
 
-app.get("/abc", (req, res) => {
-  res.send("hello abc");
-});
-
-app.listen(8080, () => {
-  console.log(`Example app listening on port ${port}`);
-  console.log(__dirname + './views/')
-});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port: ${PORT}`);
+})
