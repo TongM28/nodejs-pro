f1e688ed9f669c282cb5e840ced12ee1d5d04526
TongM28 <137702324+TongM28@users.noreply.github.com>
2025-05-22 01:05:56 +0700

    complete form MVC

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
-
+import { handleCreateUser } from "../services/user.service";
 const getHomePage = (req: Request, res: Response) => {
   return res.render("home");
-}
+};
 
 const getCreateUserPage = (req: Request, res: Response) => {
   return res.render("create-user.ejs");
-}
+};
 const postCreateUser = (req: Request, res: Response) => {
-  // console.log(">>> check data:", req.body)
+  const { fullName, email, address } = req.body;
+
+  //handle create user
+  handleCreateUser(fullName, email, address);
+
   return res.redirect("/");
-}
-export { getHomePage, getCreateUserPage,postCreateUser };
+};
+export { getHomePage, getCreateUserPage, postCreateUser };
